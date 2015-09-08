7f7850bc8b9ae711edf1a6c851b8a93cfb0b03da
Victor Moran <rvictor1991@hotmail.com>
2015-09-08 12:14:56 -0500

    Added Script HW

---
+++ Fobias/Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelloWorld : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+		print("HelloWorld");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
