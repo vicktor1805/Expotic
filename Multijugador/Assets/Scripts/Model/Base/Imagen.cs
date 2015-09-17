4449338bd4b5eb89030af3f413d5a8e03158650c
Victor Moran <vck1805@gmail.com>
2015-09-17 12:22:42 -0500

    Proyecto Multijugador

---
+++ Multijugador/Assets/Scripts/Model/Base/Imagen.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using System.Collections;
+
+public class Imagen : MonoBehaviour{
+
+    private string nombre;
+    private Sprite sprite;
+
+    void Start()
+    {
+        nombre = this.name;
+        sprite = Resources.Load<Sprite>(nombre);
+        this.GetComponent<Image>().sprite = sprite;
+        print(nombre);
+    }
+
+}
---
+++ Multijugador/Assets/Scripts/Model/Base/Row.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Row {
+
+    public List<Columna> columnas { get; set; }
+
+}
