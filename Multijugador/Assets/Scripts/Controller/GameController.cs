6ffe427b7c30ca323b218346ea77494cc160794c
Victor Moran <vck1805@gmail.com>
2015-10-22 18:15:46 -0500

    Preparing for cloud build

--- Multijugador/Assets/Scripts/Controller/GameController.cs
+++ Multijugador/Assets/Scripts/Controller/GameController.cs
@@ -5,12 +5,12 @@ using Assets.Scripts.Model.Entity;
 public class GameController : Photon.MonoBehaviour {
 
     private List<Player> listPlayers;
-    
+    public GameObject []Cameras;
 
 	void Start () {
 
         listPlayers = Util.instance.listPlayers;
-        print(listPlayers.Count);
+        InstantiateCamera();
 
 	}
 	
@@ -18,8 +18,39 @@ public class GameController : Photon.MonoBehaviour {
 	
 	}
 
-    void RelocateCameras()
+    void InstantiateCamera()
     {
-        
+        string name = PhotonNetwork.playerName;
+        print(name);
+        Player myPlayer = SearchPlapyer(name);
+
+        foreach (GameObject camera in Cameras)
+        {
+            string nameCamera = camera.name.Substring(7);
+            string stdNamePlayer = "Player" + myPlayer.id;
+
+            if (nameCamera.Equals(stdNamePlayer))
+            {
+                camera.SetActive(true);
+                break;
+            }
+
+        }
+    }
+
+    Player SearchPlapyer(string name)
+    {
+        Player myPlayer = null;
+
+        foreach(Player p in listPlayers)
+        {
+            if (p.nombre.Equals(name))
+            {
+                myPlayer = p;
+                break;
+            }
+        }
+
+        return myPlayer;
     }
 }
